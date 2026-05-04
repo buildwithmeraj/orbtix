b118a80435b45e013287840f10cabe7bb877203d
Merajul Islam <buildwithmeraj@gmail.com>
2026-05-05 00:59:52 +0600

    added a basic hero

---
+++ assets/javascripts/main.js
@@ -0,0 +1,17 @@
+new TypeIt("#typewrite", { speed: 50, loop: true })
+  .type(
+    "We build fast, scalable, and beautiful digital products that help  businesses grow in the modern web.",
+  )
+  .delete()
+  .type(
+    "Orbtix delivers cutting-edge web solutions — from pixel-perfect interfaces to robust backend systems — engineered for the future.",
+  )
+  .delete()
+  .type(
+    "Smart technology. Clean code. Real results. We turn your ideas into powerful digital experiences.",
+  )
+  .delete()
+  .type(
+    "From startups to enterprises, we craft tailored software solutions that solve real problems and drive measurable growth.",
+  )
+  .go();
